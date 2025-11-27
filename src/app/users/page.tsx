a2115b647ccf2527069be69514aca2484a9d0a4b
hirokoymj <hiroko@hirokoymj.com>
2025-11-27 14:12:01 -0600

    Removed a hard-coded localhost url

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import UserListClient from './components/UserListClient';
+import { prisma } from '../../lib/prismaClient';
 
 export default async function UserListPage() {
-  const res = await fetch('http://localhost:3000/api/users');
-  const users = await res.json();
+  //   const res = await fetch('http://localhost:3000/api/users');
+  // 	const users = await res.json();
+  const users = await prisma.user.findMany(); // direct DB query
 
   return (
     <div className="p-6">
