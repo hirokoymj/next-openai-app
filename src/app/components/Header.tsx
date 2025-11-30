8810bdaf71ab531bc966d43cc25e5cae71d06865
hirokoymj <hiroko@hirokoymj.com>
2025-11-30 06:09:38 -0600

    Updated a text and added github icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import { AiFillGithub } from 'react-icons/ai';
+
 export default function Header() {
   return (
     <header className="bg-blue-900 text-white p-4">
-      <h1>Next.js v6 + AI + TanStack Query</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">
+          Next.js 16 + OpenAI + TanStack Query Demo
+        </h1>
+
+        <Link
+          href="https://github.com/hirokoymj/next-openai-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Open source on GitHub"
+          className="text-white hover:text-gray-200 transition">
+          <AiFillGithub size={45} />
+        </Link>
+      </div>
     </header>
   );
 }
