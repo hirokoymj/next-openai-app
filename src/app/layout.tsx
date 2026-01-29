56ae532173d4d5fded8d04ee7d19956adcc72d0b
hirokoymj <hiroko@hirokoymj.com>
2026-01-29 16:12:51 -0600

    Added Force NodeJS runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
+export const runtime = 'nodejs';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
